92cef2d910c7397e946ac147a37e0763dc0620ff
aejmmark <aejmelaeus.markus@gmail.com>
2021-11-26 20:56:36 +0200

    vk4 5.4 Adding two different items sets the total of items to 2

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -19,3 +19,10 @@ class TestOstoskori(unittest.TestCase):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), 3)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkee_korissa_kaksi_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        banaani = Tuote("Banaani", 6)
+        self.kori.lisaa_tuote(banaani)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
