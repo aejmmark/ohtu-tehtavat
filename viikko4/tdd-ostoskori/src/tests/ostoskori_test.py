845cddba65a0aec662fcd8686bad1139f3905cd3
aejmmark <aejmelaeus.markus@gmail.com>
2021-11-26 20:58:24 +0200

    vk4 5.5 Adding two different items sets the price to be the sum of their prices

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -20,9 +20,16 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), 3)
 
-    def test_kahden_eri_tuotteen_lisaamisen_jalkee_korissa_kaksi_tavaraa(self):
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         banaani = Tuote("Banaani", 6)
         self.kori.lisaa_tuote(banaani)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korin_hinta_on_tuotteiden_hinta(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        banaani = Tuote("Banaani", 6)
+        self.kori.lisaa_tuote(banaani)
+        self.assertEqual(self.kori.hinta(), 9)
